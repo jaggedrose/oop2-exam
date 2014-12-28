8c3854179f092cd618376c7d0a2bfa191bd34ad0
Jagged Rose <jennierowe@hotmail.com>
2014-12-28 19:45:15 +0100

    Testing to connect to DB

--- game.php
+++ game.php
@@ -9,3 +9,22 @@ $ds = new DBObjectSaver(array(
   "password" => "mysql",
   "prefix" => "exam_game"
 ));
+
+if(!isset($ds->players)) {
+	$ds->players = array();
+}
+
+if(isset($ds->story)) {
+	$ds->story = array();
+}
+
+echo("<br>Did anything work?");
+
+if(!count($ds->players)) {
+  echo("<br>Created new player");
+  $coco = New Player("Coco");
+  $ds->players[] = $coco;
+}
+else {
+    $coco = &$ds->players[0];
+}
