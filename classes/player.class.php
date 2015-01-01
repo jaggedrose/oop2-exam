08dcfb2c7bc2c9629aca5d25fcea944a7f07722c
Jagged Rose <jennierowe@hotmail.com>
2015-01-01 11:07:53 +0100

    Made public function protected and added getters

--- classes/player.class.php
+++ classes/player.class.php
@@ -2,16 +2,40 @@
 
 class Player extends Base {
 
-	public $name;
-	public $success = 50;
+	protected $name;
+	protected $success = 50;
 
-	public $hand_stitching;
-	public $sewing;
-	public $cutting;
-	public $patterns;
+	protected $needlework;
+	protected $sewing;
+	protected $cutting;
+	protected $patterns;
 
 	public function __construct($name) {
     	$this->name = $name;
-    }
+   }
+
+	public function get_name() {
+		return $this->name;
+	}
+
+	public function get_success() {
+		return $this->success;
+	}
+
+	public function get_needlework() {
+		return $this->needlework;
+	}
+
+	public function get_sewing() {
+		return $this->sewing;
+	}
+
+	public function get_cutting() {
+		return $this->cutting;
+	}
+
+	public function get_patterns() {
+		return $this->patterns;
+	}
 	
 }
