3648fc97d89e09271c3d76e6b6e2d8e1790cbe57
Jagged Rose <jennierowe@hotmail.com>
2015-01-02 20:51:42 +0100

    Adding html with jQuery

--- js/main.js
+++ js/main.js
@@ -1,26 +1,48 @@
 $(function() {
 
 	
-	$('#addPlayer').submit(function() {
-		var form = $(this);
-		var pName = form.find('#playerName').val();
+	function choosePlayerClass() {
+		// Empty DOM "html" areas
+		$(".gameText").html('');
+		$(".gameOptions").html('');
 
-		var pClass;
+		$(".gameText").append("<h2>Create your player!</h2>");
 
-		if ($("input[name='playerClass']:checked").length > 0){
-			pClass = $('input:radio[name=playerClass]:checked').val();
-			console.log(pClass);
-		}
-		else {
-			alert("You must select a class!");
+		// Player name input
+		$(".gameOptions").append("<h3>Player Name:</h3>");
+		$(".gameOptions").append('<input id="playerName" name="playerName" type="text" placeholder="Enter your name">');
+
+		// TODO Might need this later.......
+		//find out available characters for chosen storyline
+		// var availableCharacters = storyData.available_characters;
+
+		$(".gameOptions").append("<h3>Choose a class for your player:</h3>");
+		// Player classes as radio buttons
+		$(".gameOptions").append('<input type="radio" name="playerClass" id="dressmakerClass" value="Dressmaker">');
+		$(".gameOptions").append('<input type="radio" name="playerClass" id="tailorClass" value="Tailor">');
+		$(".gameOptions").append('<input type="radio" name="playerClass" id="patternmakerClass" value="Patternmaker">');
+
+		$('#addPlayer').submit(function() {
+			var form = $(this);
+			var pName = form.find('#playerName').val();
+
+			var pClass;
+
+			if ($("input[name='playerClass']:checked").length > 0){
+				pClass = $('input:radio[name=playerClass]:checked').val();
+				console.log(pClass);
+			}
+			else {
+				alert("You must select a class!");
+				return false;
+			}
+
+			submitPlayerName(pName, pClass);
+
+			// Do not reload the page
 			return false;
-		}
-
-		submitPlayerName(pName, pClass);
-
-		// Do not reload the page
-		return false;
-	});
+		});
+	}
 
 	
 	function submitPlayerName(chosenName, chosenClass) {
